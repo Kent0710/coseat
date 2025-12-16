78bd33506244198ef300cfbb9ae2718ae3d99387
Kent0710 <christian_kent_bayani@dlsl.edu.ph>
2025-12-16 12:43:42 +0800

    feat: shadcn init

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,13 @@
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
+
 const LandingPage = () => {
     return (
         <div>
             this is the landing page
+            <Link href="/home">
+                <Button>Go to Home</Button>
+            </Link>
         </div>
     )
 };
