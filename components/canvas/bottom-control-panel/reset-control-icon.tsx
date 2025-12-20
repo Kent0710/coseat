aa9b8cad1282657b5969748b6fb7e4a912790454
Kent0710 <christian_kent_bayani@dlsl.edu.ph>
2025-12-20 11:57:21 +0800

    fix: clear bottom control not clearing blocks

--- components/canvas/bottom-control-panel/reset-control-icon.tsx
+++ components/canvas/bottom-control-panel/reset-control-icon.tsx
@@ -3,12 +3,15 @@
 import { BrushCleaning } from "lucide-react";
 import ControlIcon from "./control-icon";
 import useChairsStore from "@/store/use-chairs";
+import useBlocksStore from "@/store/use-blocks";
 
 const ResetControlIcon = () => {
     const { setChairs } = useChairsStore();
+    const { setBlocks } = useBlocksStore()
 
     const handleClick = () => {
         setChairs([]);
+        setBlocks([]);
     };
 
     return <ControlIcon icon={BrushCleaning} onClick={handleClick} />;
