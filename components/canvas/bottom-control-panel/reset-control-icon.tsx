650ed68caec918b1caf9188ceb8395bea42a1be3
Kent0710 <christian_kent_bayani@dlsl.edu.ph>
2025-12-20 12:13:14 +0800

    feat: add r keyboard shortcut for clearing all blocks and chairs

--- components/canvas/bottom-control-panel/reset-control-icon.tsx
+++ components/canvas/bottom-control-panel/reset-control-icon.tsx
@@ -4,15 +4,30 @@ import { BrushCleaning } from "lucide-react";
 import ControlIcon from "./control-icon";
 import useChairsStore from "@/store/use-chairs";
 import useBlocksStore from "@/store/use-blocks";
+import { useCallback, useEffect } from "react";
 
 const ResetControlIcon = () => {
     const { setChairs } = useChairsStore();
     const { setBlocks } = useBlocksStore()
 
-    const handleClick = () => {
+    const handleClick = useCallback(() => {
         setChairs([]);
         setBlocks([]);
-    };
+    }, [setBlocks, setChairs]);
+
+    useEffect(() => {
+        const handleKeyDown = (e: KeyboardEvent) => {
+            if (e.key === "r" || e.key === "R") {
+                handleClick();
+            }
+        };
+
+        window.addEventListener("keydown", handleKeyDown);
+
+        return () => {
+            window.removeEventListener("keydown", handleKeyDown);
+        };
+    }, [handleClick])
 
     return <ControlIcon icon={BrushCleaning} onClick={handleClick} />;
 };
